e2ab6ab2cbb2805b3e6d3ac7790b9a61685873a8
MuriloMoreir <murilo.moreira@germinare.org.br>
2024-08-16 11:03:00 -0300

    SUBINDO PASTAS LEGAIS E DIVERTIDAS GST GOSTOSO

---
+++ MURILO/murilo123.py
@@ -0,0 +1,9 @@
+# MURILO 123
+# gostoso
+
+resposta = str(input("O que o Murilo é: ")).upper().strip()
+
+if resposta == "GST":
+    print("REALMENTE O MURILO É UM GOSTOSO!")
+else:
+    print("O MURILO CONTINUA SENDO GOSTOSO!")
