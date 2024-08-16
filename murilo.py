b7a15bf9b330ad2ad10728a5ed6361e72ef50f9a
MuriloMoreir <124629928+MuriloMoreir@users.noreply.github.com>
2024-08-16 10:57:34 -0300

    Update murilo.py

--- murilo.py
+++ murilo.py
@@ -2,6 +2,6 @@ resposta = str(input("O que o Murilo é: ")).upper().strip()
 
 # PROJETO LEGAL FAZ O PROJETO
 if resposta == "GST":
-    print("REALMENTE O MURILO É UM GOSTOSO!")
+    print("REALMENTE O MURILO É UM GOSTOSO maravilhoso!")
 else:
     print("O MURILO CONTINUA SENDO GOSTOSO!")
