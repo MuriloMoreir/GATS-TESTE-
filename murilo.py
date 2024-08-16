6644884525f40815b34f048cb125db107aa68b8e
MuriloMoreir <124629928+MuriloMoreir@users.noreply.github.com>
2024-08-16 10:53:49 -0300

    UPLOAD ARQUIVO
    EEEE

---
+++ murilo.py
@@ -0,0 +1,6 @@
+resposta = str(input("O que o Murilo é: ")).upper().strip()
+
+if resposta == "GST":
+    print("REALMENTE O MURILO É UM GOSTOSO!")
+else:
+    print("O MURILO CONTINUA SENDO GOSTOSO!")
